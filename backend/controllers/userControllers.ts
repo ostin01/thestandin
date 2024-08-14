836f7111fbce1cff87db7e4d1ce3165889257776
Austin <austinpaul606@gmail.com>
2024-08-14 13:33:05 +0100

    added login and logut feature

--- backend/controllers/userControllers.ts
+++ backend/controllers/userControllers.ts
@@ -32,6 +32,38 @@ export async function signup(req: Request, res: Response) {
       return res.status(400).json({ mssg: "Invalid user data" });
     }
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ message: (error as Error).message });
+  }
+}
+
+export async function login(req: Request, res: Response) {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password as string
+    );
+
+    if (!user || !isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid login credentials" });
+
+    res.status(200).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id as unknown as string, res),
+    });
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+}
+
+export async function logoutUser(req: Request, res: Response) {
+  try {
+    res.cookie("jwt", "", { maxAge: 1 });
+    res.status(200).json({ message: "User logged out successfully" });
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
   }
 }
--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,7 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/userControllers";
+import { signup, login, logoutUser } from "../controllers/userControllers";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logoutUser);
 module.exports = router;
