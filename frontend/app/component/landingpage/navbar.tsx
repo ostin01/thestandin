6be4018f89744f9f0cc97762f553e85087fac114
Austin <austinpaul606@gmail.com>
2024-08-19 17:13:21 +0100

    frontend clean up

---
+++ frontend/app/component/landingpage/navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <section>
+      <h1>Navbar</h1>
+    </section>
+  );
+}
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { MantineProvider } from "@mantine/core";
+import { ReactQueryProvider } from "./providers/react-query";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MantineProvider>
+          {/* <ReactQueryProvider> */}
+          {children}
+          {/* </ReactQueryProvider> */}
+        </MantineProvider>
+      </body>
     </html>
   );
 }
